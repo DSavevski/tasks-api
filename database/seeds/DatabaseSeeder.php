9b7567ed3b641d64033cf3614a7bf752a82d139d
Dragan Savevski <dsavevski@Dragans-MacBook-Air.local>
2018-02-06 20:08:35 +0100

    Add readme

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,14 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         DB::table('categories')->insert([
             'id' => 1,
             'name' => 'Work',
